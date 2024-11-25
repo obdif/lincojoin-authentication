287b48afcba0fa6670038378b12ac284efbc63ed
Adekunle Blessing <adeblessinme4u@gmail.com>
2024-11-25 15:50:45 +0100

    auth commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,9 +35,9 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(",")
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 
